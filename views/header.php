1b88f0772a8cdbfcd78ca5a4458fa7151a70a972
luukk <klaverluuk@gmail.com>
2015-11-16 11:32:46 +0100

    versie 3

---
+++ views/header.php
@@ -0,0 +1,29 @@
+<div class="container">
+  <header>
+    <img src="images/radio1_logo.jpg">
+    <div id="header_top_section">
+      <a href="#">newsbrief</a>|
+      <a href="#">mobiel</a>|
+      <a href="#">contact</a>|
+      <a href="#">meld een fout</a>|
+      <a href="#">frequensies</a>|
+      <a href="#">help</a>|
+      <a href="#">rss</a>
+    </div>
+    <div id="header_menu">
+      <div id="header_tour_top_100_text">TOUR TOP 100</div>
+      <div id="header_radio1_button">
+        <a href="http://www.radio1.nl/">RADIO1.NL</a>
+      </div>
+      <div id="header_tour_top_100_button">
+        <a href="#">TOUR TOP 100</a>
+      </div>
+      <div id="header_search_button">
+        <form method="get">
+          <input type="text" name="header_search" id="header_search">
+        </form>
+      </div>
+      <d
+    </div>
+  </header>
+</div>
